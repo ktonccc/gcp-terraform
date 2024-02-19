308da57424a369b0dbf167a81b7bd23996801a2d
ktonccc <155670399+ktonccc@users.noreply.github.com>
2024-02-19 13:45:16 +0000

    cambio en fichero viariables

--- variables.tf
+++ variables.tf
@@ -1,22 +1,21 @@
 variable "client" {
 }
-
 variable "environment" {
-
 }
 variable "metadata_startup_script" {
 }
-variable "gcp_region" {
+variable "region" {
 }
-
-variable "gcp_zone" {
+variable "zone" {
 }
-
-variable "gcp_project_id" {
+variable "project_id" {
+}
+variable "machine_type" {
+}
+variable "image_family" {
 }
 variable "gcp_subnet_1" {
 }
-
 variable "gcp_vpc_name" {
 }
 variable "ssh_user" {
